b4571ca222e4a93df9bfe499a4e4e7526b26c68e
Joycesolis12 <joycesolis08@gmail.com>
2026-07-08 17:59:20 -0500

    demo: prueba de fuego milla 2

--- internal/vivienda/handlers/vivienda_test.go
+++ internal/vivienda/handlers/vivienda_test.go
@@ -172,7 +172,7 @@ func registrarYObtenerToken(t *testing.T, h http.Handler) string {
 		json.NewDecoder(rec.Body).Decode(&resp),
 	)
 
-	return resp.Token
+	return nil
 }
 
 // TestCrearVivienda_Exitoso: POST con token y cuerpo valido -> 201 Created.
